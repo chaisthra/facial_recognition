e439fded020c23e295444de74841529666367727
Chaithra <chaitra3nats@gmail.com>
2025-02-24 10:22:49 +0530

    Switch from opencv-python-headless to opencv-python

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "streamlit==1.28.0",
-        "opencv-python-headless==4.8.1.78",
+        "opencv-python==4.8.1.78",
         "face-recognition==1.3.0",
         "numpy==1.26.3",
         "Pillow==10.0.0",
